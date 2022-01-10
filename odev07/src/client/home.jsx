1afa4fa1ac2f6c6e22599a249194de21183f0859
İlayda Yegen <83966876+ilydayg@users.noreply.github.com>
2022-01-10 22:14:49 +0300

    Odev07_Son

---
+++ odev07/src/client/home.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+
+export class Home extends Component {
+    componentDidMount() {
+        if (this.props.user) {
+            this.props.fetchAndUpdateUserInfo();
+        }
+    }
+    render() {
+        const user = this.props.user;
+        const loggedIn = user !== null && user !== undefined;
+        return (
+            <div>
+                <h2>Kart Oyunu</h2>
+                <p className="welcome-text">
+                    Bu oyunda 3 kapalı kart içindeki kediyi bulman gerekmektedir.
+                    İlk tercihte Kedi kartını bulamaz isen 2. seçim hakkı tanınacaktır
+                </p>
+                <p>Online oyuncu sayısı: {this.props.userCount}</p>
+                {loggedIn ? (
+                    <div className='action'>
+                        <Link to={"/cards"} className="play">Oyna</Link>
+
+                        <p>Galibiyet: {user.victories}</p>
+                        <p>Mağlubiyet: {user.defeats}</p>
+                    </div>
+                ) : (
+                    <p>Oynamak için giriş yapmanız gerekmektedir!</p>
+                )}
+
+            </div>
+        );
+    }
+}
